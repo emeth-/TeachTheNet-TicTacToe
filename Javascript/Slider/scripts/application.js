d75f81c418340c03ee6274e96b7c1f5dccf104bf
Tom McLaughlin <tim5046@gmail.com>
2014-08-05 22:49:05 -0400

    nav buttons work. just need to add timer

--- Javascript/Slider/scripts/application.js
+++ Javascript/Slider/scripts/application.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 
 
 	var navButton = $("li").find("img");
+
 	//show the right image when the nav button is clicked
 	$(navButton).on('click',function(){
 		$("#"+currentImage).removeClass('selected');
@@ -14,14 +15,23 @@ $(document).ready(function(){
 		//change nav button when a different one is clicked
 		$('#navigation').children('li').children("img").removeClass('active');
 		$('#navigation').children('li').children("img").addClass('inactive');
+
+		$(this).removeClass('inactive');
+		$(this).addClass('active');
 	
+		$("li img.active").attr('src','images/button1.png');
+		$("li img.inactive").attr('src','images/button2.png');
+
+		//show the correct image in the slider, corresponding to the nav button
+		var navID = $(this).attr('id');
+		$('#slider').children('img').removeClass('selected');
+		$('#slider').children('img').addClass('notSelected');
+
+		$("#image-"+navID).removeClass('notSelected');
+		$("#image-"+navID).addClass('selected');
 	});
 	
-	if ($("li img").hasClass('active')){
-	    $(this).find(".active").attr('src','images/button1.png');
-	} else {
-		$(this).attr('src','images/button2.png');
-	}
+
 
 
 });
@@ -38,5 +48,8 @@ function initPics(){
 	$("#image-1").addClass('selected');
 	$("li #1").removeClass('inactive')
 	$("li #1").addClass('active');
+
+	$("li img.active").attr('src','images/button1.png');
+	$("li img.inactive").attr('src','images/button2.png');
 
 }
