bb71a22f573c49e767a979f8b7730fcf4e317a20
Tom McLaughlin <tim5046@gmail.com>
2014-08-14 14:41:00 -0400

    added bomb generation

--- Javascript/Minesweeper/scripts/application.js
+++ Javascript/Minesweeper/scripts/application.js
@@ -39,6 +39,27 @@ function draw_board(x,y){
        header.parent().last().find(".left").last().append('<td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderbr.gif"></td></tr>');   
 }
 
+function makeBombs(x,y){
+    //after the board is generated, randomly place bombs onto the board
+    var numBombs = (x*y)/10;
+    alert('Number of bombs: '+numBombs);
+    (function makeBomb(){
+        var rand=0;
+        for (var i=1;i<numBombs+1;i++){
+            rand=Math.floor(Math.random() * (x*y)) + 1;
+            var cell = $('.gameBoard').find('#'+rand);
+            if (cell.hasClass('bomb')){
+                makeBomb();
+            } else {
+                cell.addClass('bomb');   
+            }
+            rand=0;
+            
+            
+        }
+    })();
+}
+
 
 function checkCell(id){
     //this function checks whether a box that was clicked contains a bomb or not
@@ -49,7 +70,6 @@ function checkCell(id){
         $(cell).removeClass('blank');
         $(cell).text(''); 
         $(cell).addClass('clicked');
-
     }
 }
 
@@ -62,7 +82,8 @@ $(document).ready(function(){
     document.oncontextmenu = function() {return false;}; // disable right click menu
 
 
-    draw_board(20,20); 
+    draw_board(10,10); 
+    makeBombs(10,10);
     
     //play game
     //right click check
