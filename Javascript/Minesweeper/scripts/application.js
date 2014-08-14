c603be972ad3eb2ad4230a0fd8d841d7f38d54cb
Tom McLaughlin <tim5046@gmail.com>
2014-08-14 13:38:22 -0400

    added flags on right click

--- Javascript/Minesweeper/scripts/application.js
+++ Javascript/Minesweeper/scripts/application.js
@@ -13,7 +13,7 @@ function draw_board(x,y){
     
      header.parent().last().append('<tr id ="cell"><td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td>');
     for(var i=0; i<x;i++){
-        header.parent().find("#cell").last().append('<td class="cell" id='+i+'><img src="http://www.chezpoor.com/minesweeper/images/blank.gif"</td>');
+        header.parent().find("#cell").last().append('<td class="cell blank" id='+i+'><img src="http://www.chezpoor.com/minesweeper/images/blank.gif"</td>');
     }
     header.parent().find("#cell").last().append('<td class="right" id="cell"><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td></tr>');
 
@@ -21,7 +21,7 @@ function draw_board(x,y){
     for(var j=0;j<y-1;j++){
          header.parent().last().append('<tr class="left" id ="cell"><td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td>');
         for(var i=0; i<x;i++){
-            header.parent().last().find(".left").last().append('<td class="cell" id='+i+'><img src="http://www.chezpoor.com/minesweeper/images/blank.gif"</td>');
+            header.parent().last().find(".left").last().append('<td class="cell blank" id='+i+'><img src="http://www.chezpoor.com/minesweeper/images/blank.gif"></td>');
         }
         header.parent().last().find(".left").last().append('<td id="right"><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td></tr>');    
     }
@@ -32,17 +32,39 @@ function draw_board(x,y){
             //this adds in the border
             header.parent().last().find(".left").last().append('<td><img id="bottomBorder" src="http://www.chezpoor.com/minesweeper/images/bordertb.gif"></td>');
         }
-       header.parent().last().find(".left").last().append('<td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderbr.gif"></td></tr>');
-    
-    
-    
-    
-    
-    
-    
+       header.parent().last().find(".left").last().append('<td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderbr.gif"></td></tr>');   
 }
 
+
+
+
+
 $(document).ready(function(){
+    
+        
+    document.oncontextmenu = function() {return false;}; // disable right click menu
+
 
     draw_board(10,10); 
+    
+    //play game
+    //right click check
+    $(".left").find('td').on('mousedown',function(e){
+      if( e.button == 2 ) {
+          if($(this).hasClass('blank')){
+              $(this).removeClass('blank');
+              $(this).addClass('flag');
+              $(this).text('');
+              $(this).append('<img src="http://www.chezpoor.com/minesweeper/images/bombflagged.gif">'); //add flag if it's blank
+          } else {
+              $(this).removeClass('flag');
+              $(this).addClass('blank');
+              $(this).text('');
+              $(this).append('<img src="http://www.chezpoor.com/minesweeper/images/blank.gif">'); //back to blank
+          }
+      }
+    });
+   
+    
+    
 });
