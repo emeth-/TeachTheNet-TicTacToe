40e2ac1249ec963670ce414b6700dff35dff2748
Tom McLaughlin <tim5046@gmail.com>
2014-08-14 12:50:39 -0400

    board builder works. need to refactor

---
+++ Javascript/Minesweeper/scripts/application.js
@@ -0,0 +1,48 @@
+function draw_board(x,y){
+    //first draw the background
+    var header=$('.gameBoard').find("#header");
+    //http://www.chezpoor.com/minesweeper/images/bordertr.gif
+    header.append('<td><img id="tableBorder" src="http://www.chezpoor.com/minesweeper/images/bordertl.gif"></td>');
+    for (var i=0; i<x; i++){
+        //this adds in the border
+        header.append('<td><img id="tableBorder" src="http://www.chezpoor.com/minesweeper/images/bordertb.gif"></td>');
+    }
+       header.append('<td><img id="tableBorder" src="http://www.chezpoor.com/minesweeper/images/bordertr.gif"></td></tr>');
+    
+    //header row finished above. Now draw the actual cells
+    
+     header.parent().last().append('<tr id ="cell"><td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td>');
+    for(var i=0; i<x;i++){
+        header.parent().find("#cell").last().append('<td class="cell" id='+i+'><img src="http://www.chezpoor.com/minesweeper/images/blank.gif"</td>');
+    }
+    header.parent().find("#cell").last().append('<td class="right" id="cell"><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td></tr>');
+
+    //draw body
+    for(var j=0;j<y-1;j++){
+         header.parent().last().append('<tr class="left" id ="cell"><td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td>');
+        for(var i=0; i<x;i++){
+            header.parent().last().find(".left").last().append('<td class="cell" id='+i+'><img src="http://www.chezpoor.com/minesweeper/images/blank.gif"</td>');
+        }
+        header.parent().last().find(".left").last().append('<td id="right"><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderlr.gif"></td></tr>');    
+    }
+    
+    //draw bottom border
+        header.parent().last().append('<tr class="left" id ="cell"><td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderbl.gif"></td>'); 
+        for (var i=0; i<x; i++){
+            //this adds in the border
+            header.parent().last().find(".left").last().append('<td><img id="bottomBorder" src="http://www.chezpoor.com/minesweeper/images/bordertb.gif"></td>');
+        }
+       header.parent().last().find(".left").last().append('<td><img id="leftRightBorder" src="http://www.chezpoor.com/minesweeper/images/borderbr.gif"></td></tr>');
+    
+    
+    
+    
+    
+    
+    
+}
+
+$(document).ready(function(){
+
+    draw_board(10,10); 
+});
